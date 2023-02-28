508f4bac9542f4da1394bbb6dfc03b695014bf12
rishavmishra2051 <114403095+rishavmishra2051@users.noreply.github.com>
2023-02-28 20:52:13 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_POST['name'];
+$visitor_email= $_POST['email'];
+$message= $_POST['message'];
+$email_from = 'rishavmishra2051@gmail.com';
+$email_subject = 'New Form';
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+                "User Message: $message.\n";
+$to = "rishavmishrarm2001@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:thankyou.html");
+?>
